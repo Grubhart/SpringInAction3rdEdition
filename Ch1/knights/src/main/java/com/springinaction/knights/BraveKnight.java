39d48a343af20a7f0cbada8cb96b2907ce311c5b
grubhart <grubhart@gmail.com>
2016-11-23 18:32:03 -0500

    added aop support

--- Ch1/knights/src/main/java/com/springinaction/knights/BraveKnight.java
+++ Ch1/knights/src/main/java/com/springinaction/knights/BraveKnight.java
@@ -7,20 +7,17 @@ import com.springinaction.quests.Quest;
 public class BraveKnight implements Knight{
 
     private Quest quest;
-    private Minstrel minstrel;
-
-
 
 
     public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest = quest;
-        this.minstrel= minstrel;
+
     }
 
     public void embarkOnQuest()throws QuestException{
-        minstrel.singBeforeQuest();
+
         quest.embark();
-        minstrel.singAfterQuest();
+
     }
 }
 
