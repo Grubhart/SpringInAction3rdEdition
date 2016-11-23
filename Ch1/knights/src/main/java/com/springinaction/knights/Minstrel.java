5410bbce2c807878a8416e1699071cf5f7496543
grubhart <grubhart@gmail.com>
2016-11-23 17:12:22 -0500

    first attempt to use cross cutting method without AOP

---
+++ Ch1/knights/src/main/java/com/springinaction/knights/Minstrel.java
@@ -0,0 +1,17 @@
+package com.springinaction.knights;
+
+/**
+ * Created by Edson on 22/11/2016.
+ */
+public class Minstrel {
+
+    public void singBeforeQuest(){
+        System.out.println("Fa la the Knight is so brave");
+
+    }
+
+    public void singAfterQuest(){
+        System.out.println("tee hee the knight embark on a quest");
+    }
+
+}
