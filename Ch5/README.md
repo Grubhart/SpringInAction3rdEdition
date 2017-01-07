02ddcf7a436701ba67d47f525abaeaccb0c0fa32
grubhart <grubhart@gmail.com>
2017-01-07 02:27:14 -0500

    added select and insert operations

--- Ch5/README.md
+++ Ch5/README.md
@@ -4,7 +4,7 @@ inside hsqldb folder you can found the files to start hsqldb server, just execut
 java -cp <hsqldb.jar path> org.hsqldb.server.Server --database.0 file:spitter --dbname.0 spitter
 ```
 
-also i'm using dbcp2 that means that you need use jdk 7 + to compile or change the dependency to dbcp and the datasource property from
+also i'm using dbcp2 that means that you need use jdk 7 (or above) or change the dependency to dbcp and the datasource property from
 
 ```
 <property name="maxTotal" value="10"/>
--- Ch5/spitter_jdbcTemplate/src/main/java/com/habuma/spitter/persistence/SimpleJdbcTemplateSpitterDao.java
+++ Ch5/spitter_jdbcTemplate/src/main/java/com/habuma/spitter/persistence/SimpleJdbcTemplateSpitterDao.java
@@ -3,9 +3,13 @@ package com.habuma.spitter.persistence;
 
 import com.habuma.domain.spitter.persistence.Spitter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class SimpleJdbcTemplateSpitterDao extends SimpleJdbcDaoSupport {
@@ -13,6 +17,9 @@ public class SimpleJdbcTemplateSpitterDao extends SimpleJdbcDaoSupport {
     private JdbcTemplate jdbcTemplate;
 
     private static final String SQL_INSERT_SPITTER = "insert into spitter (username, password, fullname, email, update_by_email) values (?, ?, ?, ?, ?)";
+    private static final String SQL_SELECT_SPITTER = "select id, username, password, fullname from spitter";
+    private static final String SQL_SELECT_SPITTER_BY_ID = SQL_SELECT_SPITTER + " where id=?";
+
 
     public void addSpitter(Spitter spitter){
 
@@ -20,7 +27,7 @@ public class SimpleJdbcTemplateSpitterDao extends SimpleJdbcDaoSupport {
                             spitter.getUsername(),
                             spitter.getPassword(),
                             spitter.getFullname(),
-                            spitter.getEmail(),
+                            spitter.getEmail(),`
                             spitter.isUpdatedByEmail());
         spitter.setId(queryForIdentity());
 
@@ -33,4 +40,18 @@ public class SimpleJdbcTemplateSpitterDao extends SimpleJdbcDaoSupport {
         return getSimpleJdbcTemplate().queryForLong("call identity()");
     }
 
+    public Spitter getSpitterById(long id){
+        return jdbcTemplate.queryForObject(SQL_SELECT_SPITTER_BY_ID, new ParameterizedRowMapper<Spitter>() {
+            public Spitter mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+
+                Spitter spitter = new Spitter();
+                spitter.setId(resultSet.getLong(1));
+                spitter.setUsername(resultSet.getString(2));
+                spitter.setPassword(resultSet.getString(3));
+                spitter.setFullname(resultSet.getString(4));
+
+                return spitter;
+            }
+        },id);
+    }
 }
